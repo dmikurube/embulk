48bc683a8de31666262bf6d2982c545483f9bd2e
Dai MIKURUBE <dmikurube@treasure-data.com>
2018-07-04 17:29:28 +0900

    [WIP] Refactor

--- embulk-standards/src/main/java/org/embulk/standards/LocalFileInputPlugin.java
+++ embulk-standards/src/main/java/org/embulk/standards/LocalFileInputPlugin.java
@@ -1,15 +1,15 @@
 package org.embulk.standards;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileVisitOption;
 import java.nio.file.FileVisitResult;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
@@ -56,32 +57,30 @@ public class LocalFileInputPlugin implements FileInputPlugin {
 
     private final Logger log = Exec.getLogger(getClass());
 
-    private static final Path CURRENT_DIR = Paths.get(".").normalize();
+    private static final Path CURRENT_DIR = Paths.get("").normalize();
 
     @Override
-    public ConfigDiff transaction(ConfigSource config, FileInputPlugin.Control control) {
-        PluginTask task = config.loadConfig(PluginTask.class);
+    public ConfigDiff transaction(final ConfigSource config, final FileInputPlugin.Control control) {
+        final PluginTask task = config.loadConfig(PluginTask.class);
 
         // list files recursively
-        List<String> files = listFiles(task);
+        final List<String> files = listFiles(task);
         log.info("Loading files {}", files);
         task.setFiles(files);
 
         // number of processors is same with number of files
-        int taskCount = task.getFiles().size();
+        final int taskCount = task.getFiles().size();
         return resume(task.dump(), taskCount, control);
     }
 
     @Override
-    public ConfigDiff resume(TaskSource taskSource,
-            int taskCount,
-            FileInputPlugin.Control control) {
-        PluginTask task = taskSource.loadTask(PluginTask.class);
+    public ConfigDiff resume(final TaskSource taskSource, final int taskCount, final FileInputPlugin.Control control) {
+        final PluginTask task = taskSource.loadTask(PluginTask.class);
 
         control.run(taskSource, taskCount);
 
         // build next config
-        ConfigDiff configDiff = Exec.newConfigDiff();
+        final ConfigDiff configDiff = Exec.newConfigDiff();
 
         // last_path
         if (task.getFiles().isEmpty()) {
@@ -99,51 +98,62 @@ public class LocalFileInputPlugin implements FileInputPlugin {
     }
 
     @Override
-    public void cleanup(TaskSource taskSource,
-            int taskCount,
-            List<TaskReport> successTaskReports) {}
+    public void cleanup(final TaskSource taskSource, final int taskCount, final List<TaskReport> successTaskReports) {}
 
-    public List<String> listFiles(PluginTask task) {
-        Path pathPrefix = Paths.get(task.getPathPrefix()).normalize();
-        final Path directory;
+    public List<String> listFiles(final PluginTask task) {
+        final Path pathPrefixResolved = CURRENT_DIR.resolve(Paths.get(task.getPathPrefix())).normalize();
+        final Path dirToStartWalking;
+        final PathMatcher baseFileNameMatcher;
         final String fileNamePrefix;
-        if (Files.isDirectory(pathPrefix)) {
-            directory = pathPrefix;
+        if (Files.isDirectory(pathPrefixResolved)) {
+            dirToStartWalking = pathPrefixResolved;
             fileNamePrefix = "";
+            baseFileNameMatcher = buildPathMatcherForBaseFileName("");
         } else {
-            fileNamePrefix = pathPrefix.getFileName().toString();
-            Path d = pathPrefix.getParent();
-            directory = (d == null ? CURRENT_DIR : d);
+            dirToStartWalking = Optional.ofNullable(pathPrefixResolved.getParent()).orElse(CURRENT_DIR);
+            fileNamePrefix = pathPrefixResolved.getFileName().toString();
+            baseFileNameMatcher = buildPathMatcherForBaseFileName(fileNamePrefix);
         }
+        final PathMatcher dirToStartWalkingMatcher = buildPathMatcherForDirectory(dirToStartWalking);
 
-        final ImmutableList.Builder<String> builder = ImmutableList.builder();
-        final String lastPath = task.getLastPath().orNull();
+        final ArrayList<String> fileListBuilt = new ArrayList<>();
+        final String lastPath = task.getLastPath().orElse(null);
         try {
-            log.info("Listing local files at directory '{}' filtering filename by prefix '{}'", directory.equals(CURRENT_DIR) ? "." : directory.toString(), fileNamePrefix);
+            log.info("Listing local files at directory '{}' filtering filename by prefix '{}'",
+                     dirToStartWalking.equals(CURRENT_DIR) ? "." : dirToStartWalking.toString(),
+                     fileNamePrefix);
 
-            int maxDepth = Integer.MAX_VALUE;
-            Set<FileVisitOption> opts;
+            final Set<FileVisitOption> visitOptions;
             if (task.getFollowSymlinks()) {
-                opts = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+                visitOptions = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
             } else {
-                opts = EnumSet.noneOf(FileVisitOption.class);
+                visitOptions = EnumSet.noneOf(FileVisitOption.class);
                 log.info("\"follow_symlinks\" is set false. Note that symbolic links to directories are skipped.");
             }
 
-            Files.walkFileTree(directory, opts, maxDepth, new SimpleFileVisitor<Path>() {
+            Files.walkFileTree(dirToStartWalking, visitOptions, Integer.MAX_VALUE, new SimpleFileVisitor<Path>() {
                     @Override
-                    public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) {
-                        if (path.equals(directory)) {
+                    public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) {
+                        // NOTE: |dir| contains |dirToStartWalking|.
+                        if (dir.equals(dirToStartWalking)) {
                             return FileVisitResult.CONTINUE;
-                        } else if (lastPath != null && path.toString().compareTo(lastPath) <= 0) {
+                        } else if (lastPath != null && dir.toString().compareTo(lastPath) <= 0) {
+                            return FileVisitResult.SKIP_SUBTREE;
+                        } else if (!dirToStartWalkingMatcher.matches(dir)) {
+                            // This condition looks to be always false, but it can be true for example in Mac OS X.
+                            // It is due to the difference of |SimpleFileVisitor| and |PathMatcher|.
+                            //
+                            // |SimpleFileVisitor| visits files under a directory which matches |dirToStartWalking|
+                            // in a case-insensitive manner in popular file systems on Mac OS X. On the other hand,
+                            // |PathMatcher| always matches in a case-sensitive manner in Mac OS X.
+                            //
+                            // To match consistently through the entire path specified in user's configuration,
+                            // paths that don't match with |PathMatcher| are explicitly rejected here.
                             return FileVisitResult.SKIP_SUBTREE;
                         } else {
-                            Path parent = path.getParent();
-                            if (parent == null) {
-                                parent = CURRENT_DIR;
-                            }
-                            if (parent.equals(directory)) {
-                                if (path.getFileName().toString().startsWith(fileNamePrefix)) {
+                            final Path parent = Optional.ofNullable(dir.getParent()).orElse(CURRENT_DIR);
+                            if (parent.equals(dirToStartWalking)) {
+                                if (baseFileNameMatcher.matches(dir.getFileName())) {
                                     return FileVisitResult.CONTINUE;
                                 } else {
                                     return FileVisitResult.SKIP_SUBTREE;
@@ -155,45 +165,55 @@ public class LocalFileInputPlugin implements FileInputPlugin {
                     }
 
                     @Override
-                    public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) {
+                        // NOTE: |file| contains |dirToStartWalking|.
                         try {
                             // Avoid directories from listing.
                             // Directories are normally unvisited with |FileVisitor#visitFile|, but symbolic links to
                             // directories are visited like files unless |FOLLOW_LINKS| is set in |Files#walkFileTree|.
                             // Symbolic links to directories are explicitly skipped here by checking with |Path#toReadlPath|.
-                            if (Files.isDirectory(path.toRealPath())) {
+                            if (Files.isDirectory(file.toRealPath())) {
                                 return FileVisitResult.CONTINUE;
                             }
-                        } catch (IOException ex) {
+                        } catch (final IOException ex) {
                             throw new RuntimeException("Can't resolve symbolic link", ex);
                         }
-                        if (lastPath != null && path.toString().compareTo(lastPath) <= 0) {
+                        if (lastPath != null && file.toString().compareTo(lastPath) <= 0) {
+                            return FileVisitResult.CONTINUE;
+                        } else if (!dirToStartWalkingMatcher.matches(file)) {
+                            // This condition looks to be always false, but it can be true for example in Mac OS X.
+                            // It is due to the difference of |SimpleFileVisitor| and |PathMatcher|.
+                            //
+                            // |SimpleFileVisitor| visits files under a directory which matches |dirToStartWalking|
+                            // in a case-insensitive manner in popular file systems on Mac OS X. On the other hand,
+                            // |PathMatcher| always matches in a case-sensitive manner in Mac OS X.
+                            //
+                            // To match consistently through the entire path specified in user's configuration,
+                            // paths that don't match with |PathMatcher| are explicitly rejected here.
                             return FileVisitResult.CONTINUE;
                         } else {
-                            Path parent = path.getParent();
-                            if (parent == null) {
-                                parent = CURRENT_DIR;
-                            }
-                            if (parent.equals(directory)) {
-                                if (path.getFileName().toString().startsWith(fileNamePrefix)) {
-                                    builder.add(path.toString());
+                            final Path parent = Optional.ofNullable(file.getParent()).orElse(CURRENT_DIR);
+                            if (parent.equals(dirToStartWalking)) {
+                                if (baseFileNameMatcher.matches(file.getFileName())) {
+                                    fileListBuilt.add(file.toString());
                                     return FileVisitResult.CONTINUE;
                                 }
                             } else {
-                                builder.add(path.toString());
+                                fileListBuilt.add(file.toString());
                             }
                             return FileVisitResult.CONTINUE;
                         }
                     }
                 });
-        } catch (IOException ex) {
-            throw new RuntimeException(String.format("Failed get a list of local files at '%s'", directory), ex);
+        } catch (final IOException ex) {
+            throw new RuntimeException(
+                    String.format("Failed get a list of local files at '%s'", dirToStartWalking), ex);
         }
-        return builder.build();
+        return Collections.unmodifiableList(fileListBuilt);
     }
 
     @Override
-    public TransactionalFileInput open(TaskSource taskSource, int taskIndex) {
+    public TransactionalFileInput open(final TaskSource taskSource, final int taskIndex) {
         final PluginTask task = taskSource.loadTask(PluginTask.class);
 
         final File file = new File(task.getFiles().get(taskIndex));
@@ -213,5 +233,45 @@ public class LocalFileInputPlugin implements FileInputPlugin {
                 return Exec.newTaskReport();
             }
         };
+    }
+
+    private static PathMatcher buildPathMatcherForDirectory(final Path dir) {
+        final StringBuilder builder = buildGlobPatternStringBuilder(dir.toString());
+
+        if (builder.charAt(builder.length() - 1) != File.separatorChar) {
+            if (File.separatorChar == '\\') {
+                builder.append('\\');
+            }
+            builder.append(File.separator);
+        }
+        return FileSystems.getDefault().getPathMatcher("glob:" + builder.toString() + "**");
+    }
+
+    private static PathMatcher buildPathMatcherForBaseFileName(final String baseFileName) {
+        final StringBuilder builder = buildGlobPatternStringBuilder(baseFileName);
+        return FileSystems.getDefault().getPathMatcher("glob:" + builder.toString() + "*");
+    }
+
+    private static StringBuilder buildGlobPatternStringBuilder(final String pathString) {
+        final StringBuilder builder = new StringBuilder();
+        // Escape the special characters for the FileSystem#getPathMatcher().
+        // See: https://docs.oracle.com/javase/8/docs/api/java/nio/file/FileSystem.html#getPathMatcher-java.lang.String-
+        pathString.chars().forEach(c -> {
+                switch ((char) c) {
+                    case '*':
+                    case '?':
+                    case '{':
+                    case '}':
+                    case '[':
+                    case ']':
+                    case '\\':
+                        builder.append('\\');
+                        break;
+                    default:
+                        break;
+                }
+                builder.append((char) c);
+            });
+        return builder;
     }
 }
